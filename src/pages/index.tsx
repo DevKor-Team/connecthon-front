842568d9b401eee1980df470405f4f84a766d030
Seungwoo Lee <timosean@naver.com>
2022-07-05 13:27:33 +0900

    feat: apply responsive font sizes to headings

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-    return <h1>Hello DevKor</h1>;
+    return (
+        <>
+            <h1>Hello DevKor</h1>
+            <h2>Hello DevKor</h2>
+            <h3>Hello DevKor</h3>
+        </>
+    );
 };
 
 export default Home;
