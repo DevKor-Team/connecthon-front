2cde20c1691fae2fabf91ebf94a987301f30ebbc
aiccuracy <soojin.emily@gmail.com>
2022-07-06 20:53:38 +0900

    fix: conflicts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
 import type { NextPage } from 'next';
 import Header from '../components/Header';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
     return (
         <div>
+            <Head>
+                <title>KU HACKATHON</title>
+                <link rel="icon" href="/symbol.ico" />
+            </Head>
             <Header />
         </div>
     );
