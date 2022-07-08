7552c04facc03b46bc95a83fdb7a8a6a47a77e65
aiccuracy <soojin.emily@gmail.com>
2022-07-08 14:07:04 +0900

    fix: change tailwind config(color)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,26 +2,13 @@
 module.exports = {
     content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        colors: {
-            ourBlack: '#1D1D1D',
-            ourWhite: '#F8F8F8',
-        },
         extend: {
             fontFamily: {
                 impact: ['impact'],
             },
-            keyframes: {
-                textanimation: {
-                    '0%, 100%': {
-                        opacity: 1,
-                    },
-                    '50%': {
-                        opacity: 0,
-                    },
-                },
-            },
-            animation: {
-                textAnimation: 'textAnimation 1s ease-in-out infinite',
+            colors: {
+                ourBlack: '#1D1D1D',
+                outWhite: '#F8F8F8',
             },
         },
     },
