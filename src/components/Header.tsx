dd1e58f8afc90b4771e54752244b36fab5cfde98
Seungwoo Lee <timosean@naver.com>
2022-07-12 15:51:04 +0900

    feat: add header dark theme

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import { FiMenu } from 'react-icons/fi';
 import { MdOutlineClose } from 'react-icons/md';
 import Link from 'next/link';
 
-function SideMenu() {
+function SideMenu({ theme }: { theme?: 'dark' | 'light' }) {
     function closeMenu() {
         if (typeof window !== 'undefined') {
             const sidebg = document.getElementById('sidebg');
@@ -20,8 +20,8 @@ function SideMenu() {
     return (
         <div className="fixed inset-0 z-[110] w-[100vw] invisible md:hidden" id="wrapper">
             <div className="fixed invisible bg-black/0 w-[100vw] inset-0 transition-all duration-400" id="sidebg">
-                <div className="fixed -left-[100vw] transition-[left] duration-500 w-80 h-full bg-white px-4 pt-6 shadow-lg" id="sidebar">
-                    <MdOutlineClose className="absolute top-9 right-4" size={20} onClick={closeMenu} />
+                <div className="fixed -left-[100vw] transition-[left] duration-500 w-80 h-full bg-white px-4 pt-2 shadow-lg" id="sidebar">
+                    <MdOutlineClose className="absolute top-5 right-4" size={20} onClick={closeMenu} />
                     <div className="font-impact border-b border-slate-200 py-2 mb-7 text-xl">KU HACKATHON</div>
                     <ul className="font-bold space-y-8">
                         <li>
@@ -59,14 +59,14 @@ function Header({ theme }: { theme?: 'dark' | 'light' }) {
 
     return (
         <>
-            <SideMenu />
+            <SideMenu theme={theme} />
             <header className={`z-[100] ${theme == 'dark' ? 'bg-ourBlack' : ''}`}>
-                <div className="mt-5 ml-1 flex z-70 items-center space-x-6 md:space-x-16">
-                    <FiMenu size={24} className="text-lg font-light md:hidden cursor-pointer" onClick={openMenu} />
+                <div className="ml-1 flex z-70 items-center space-x-6 md:space-x-16">
+                    <FiMenu size={24} stroke={`${theme == 'dark' ? 'white' : 'black'}`} className={`text-lg font-light md:hidden cursor-pointer`} onClick={openMenu} />
                     <Link href="/">
                         <button className={`${theme == 'dark' ? 'text-ourWhite' : ''} font-normal font-impact text-xl md:text-2xl md:mr-6`}>KU HACKATHON</button>
                     </Link>
-                    <ul className="hidden space-x-4 md:flex md:space-x-6 lg:space-x-8 font-semibold">
+                    <ul className={`${theme == 'dark' ? 'text-ourWhite' : ''} hidden space-x-4 md:flex md:space-x-6 lg:space-x-8 font-semibold`}>
                         <li>
                             <Link href="/about">ABOUT</Link>
                         </li>
@@ -82,9 +82,9 @@ function Header({ theme }: { theme?: 'dark' | 'light' }) {
                     </ul>
                 </div>
 
-                <div className="flex items-center font-light mt-4 mr-4 cursor-pointer md:mr-12">
+                <div className="flex items-center font-light mr-4 cursor-pointer md:mr-12">
                     <Link href="/mypage">
-                        <BsPerson size={22} />
+                        <BsPerson size={22} fill={`${theme == 'dark' ? 'white' : 'black'}`} />
                     </Link>
                 </div>
             </header>
