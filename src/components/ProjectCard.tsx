6abe6f449b2d28db7d71619c0f57dc41c6254db6
Seungwoo Lee <timosean@naver.com>
2022-07-15 00:36:03 +0900

    feat: complete MainProject part with animation, mobile view

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,8 +1,80 @@
+import { MouseEvent } from 'react';
+
+function attachScaling(e: MouseEvent<HTMLDivElement>) {
+    //for collecting siblings
+    const siblings: HTMLElement[] = [];
+
+    let target;
+    if (e.target instanceof HTMLDivElement) target = e.target;
+
+    if (typeof target == 'undefined') return;
+
+    //if no parent, return
+    if (!target.parentElement) {
+        return;
+    }
+
+    console.log('mouse over');
+
+    //first child of the parent element
+    let currentChild = target.parentElement.firstChild;
+
+    //collecting siblings
+    if (currentChild instanceof HTMLDivElement) {
+        while (currentChild) {
+            if (currentChild.nodeType === 1 && currentChild !== target) {
+                siblings.push(currentChild);
+            }
+
+            currentChild = currentChild.nextSibling;
+        }
+    }
+    target.classList.add('sm:scale-110', 'sm:shadow-[7px_7px_18px_2px_rgba(0,0,0,0.1)]');
+    siblings.forEach(sib => sib.classList.add('sm:scale-75'));
+}
+
+function detachScaling(e: MouseEvent<HTMLDivElement>) {
+    //for collecting siblings
+    const siblings: HTMLElement[] = [];
+
+    let target;
+    if (e.target instanceof HTMLDivElement) target = e.target;
+
+    if (typeof target == 'undefined') return;
+
+    //if no parent, return
+    if (!target.parentElement) {
+        return;
+    }
+
+    //first child of the parent element
+    let currentChild = target.parentElement.firstChild;
+
+    //collecting siblings
+    if (currentChild instanceof HTMLDivElement) {
+        while (currentChild) {
+            if (currentChild.nodeType === 1 && currentChild !== target) {
+                siblings.push(currentChild as HTMLElement);
+            }
+
+            currentChild = currentChild.nextSibling;
+        }
+    }
+
+    target.classList.remove('sm:scale-110', 'sm:shadow-[7px_7px_18px_2px_rgba(0,0,0,0.1)]');
+    siblings.forEach(sib => sib.classList.remove('sm:scale-75'));
+    console.log('mouse leave');
+}
+
 export default function ProjectCard({ imgurl, title, team }: { imgurl?: string; title: string; team: string }) {
     return (
-        <div className="w-full md:w-1/3 rounded-xl shadow-[1px_1px_3px_2px_rgba(232,237,237,1)]">
-            <div className="bg-sky-100 w-full h-56 md:h-60 lg:h-64 xl:h-80 2xl:h-96 rounded-t-xl">프로젝트대표사진</div>
-            <div className="bg-ourWhite w-full h-[5.5rem] px-10 lg:px-6 py-4 rounded-b-xl flex flex-col items-start justify-around">
+        <div
+            className="transition-all ease-out duration-300 w-full md:w-1/3 rounded-xl shadow-[1px_1px_3px_2px_rgba(232,237,237,1)]"
+            onMouseOver={e => attachScaling(e)}
+            onMouseLeave={e => detachScaling(e)}
+        >
+            <div className="bg-sky-100 w-full h-56 md:h-60 lg:h-64 xl:h-80 2xl:h-96 rounded-t-xl pointer-events-none">프로젝트대표사진</div>
+            <div className="bg-ourWhite w-full h-[5.5rem] px-10 lg:px-6 py-4 rounded-b-xl flex flex-col items-start justify-around pointer-events-none">
                 <h1 className="font-bold text-lg sm:text-base lg:text-lg">{title}</h1>
                 <h2 className="font-semibold text-base sm:text-sm lg:text-base">{team}</h2>
             </div>
