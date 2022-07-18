1e848a91c84f3dc2ea86a28610ed7997ea2f6e18
Seungwoo Lee <timosean@naver.com>
2022-07-18 19:31:25 +0900

    fix: all needed changes for common paddings carried out

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -10,7 +10,7 @@ const Layout: React.FC = ({ children }) => {
                 <link rel="icon" href="/symbol-2d.svg" />
             </Head>
             <Header />
-            <div>{children}</div>
+            <div className="px-4 md:px-16 lg:px-20 xl:px-[13.375rem]">{children}</div>
             <Footer />
         </div>
     );
