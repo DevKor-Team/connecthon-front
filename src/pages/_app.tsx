f7f433d64a3efe0efd7391b114bee45cbc83a55c
aiccuracy <soojin.emily@gmail.com>
2022-08-11 03:19:21 +0900

    test: test for production

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,11 @@
 import '../../styles/globals.css';
 import type { AppProps } from 'next/app';
-import Layout from '../layouts/Layout';
-import { CustomNextPage } from '../types/types';
-import { Fragment } from 'react';
-import { ParallaxProvider } from 'react-scroll-parallax';
 import { RecoilRoot } from 'recoil';
 
 function HACKATHON({ Component, pageProps }: AppProps) {
-    const page = Component as CustomNextPage;
-    const Layout = page.Layout ?? Fragment;
     return (
         <RecoilRoot>
-            <Layout>
-                <ParallaxProvider scrollAxis="vertical">
-                    <Component {...pageProps} />
-                </ParallaxProvider>
-            </Layout>
+            <Component {...pageProps} />
         </RecoilRoot>
     );
 }
