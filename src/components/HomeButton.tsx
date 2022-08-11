f99a44a7ebcc2c3ceb4efc44bde9ce465bd063bf
aiccuracy <soojin.emily@gmail.com>
2022-08-12 00:55:44 +0900

    fix: change declaration and type error

--- src/components/HomeButton.tsx
+++ src/components/HomeButton.tsx
@@ -12,9 +12,9 @@ const sizes = {
     lg: 'w-64 h-[3.75rem] text-2xl',
 };
 
-export default function HomeButton({ size, className, children }: BtnType) {
-    let btnSize = sizes[size];
-    let shadowStyle = 'shadow-[0px_5px_20px_5px_rgba(17,17,26,0.4)]';
+export default function HomeButton({ size = 'md', className, children }: BtnType) {
+    const btnSize = sizes[size];
+    const shadowStyle = 'shadow-[0px_5px_20px_5px_rgba(17,17,26,0.4)]';
 
     return (
         <Link href="/about">
