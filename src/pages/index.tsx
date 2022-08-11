957379b7bcde66b77a580eabc650a93ad343b08f
aiccuracy <soojin.emily@gmail.com>
2022-08-12 00:35:33 +0900

    refactor: remove layout and unnecessary code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
 import type { NextPage } from 'next';
-import Header from '../components/Header';
 import Head from 'next/head';
-import Layout from '../layouts/Layout';
 import { CustomNextPage } from '../types/types';
 import Landing from './landing';
 import { axiosInstance } from '../hooks/queries';
@@ -38,42 +36,11 @@ const Home: CustomNextPage = () => {
         getSessionUser();
     });
 
-    // useEffect(() => {
-    //     setUserData({
-    //         isLogin: true,
-    //         user: {
-    //             id: 1,
-    //             name: '안수진',
-    //             email: 'asj0816@korea.ac.kr',
-    //             image: '/soojin.png',
-    //             position: 'developer',
-    //             teamName: 'Tiger',
-    //             profile: {
-    //                 link: {
-    //                     github: 'aiccuracy',
-    //                     blog: 'www.hojins.life',
-    //                     instagram: '@10issoojin_',
-    //                 },
-    //                 img: '/soojin.png',
-    //                 introduction: '뎁코 화이팅',
-    //                 university: '고려대학교',
-    //                 major: '국제학부',
-    //                 career: ['코르카 ML 엔지니어', 'DevKor 노예'],
-    //             },
-    //         },
-    //     });
-    // }, []);
-
     return (
         <div>
-            <Head>
-                <title>KU HACKATHON</title>
-                <link rel="icon" href="/symbol.ico" />
-            </Head>
             <Landing />
         </div>
     );
 };
 
-Home.Layout = Layout;
 export default Home;
