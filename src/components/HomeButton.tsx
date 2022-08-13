7f85903e3dea2ac7382bb0b461a360e8f419e649
Seungwoo Lee <timosean@naver.com>
2022-08-13 15:12:03 +0900

    fix: homebutton UI to About Page

--- src/components/HomeButton.tsx
+++ src/components/HomeButton.tsx
@@ -7,9 +7,9 @@ interface BtnType {
 }
 
 const sizes = {
-    sm: 'w-44 h-10 text-lg',
-    md: 'w-52 h-12 text-xl',
-    lg: 'w-64 h-[3.75rem] text-2xl',
+    sm: 'w-44 h-10',
+    md: 'w-52 h-12',
+    lg: 'w-64 h-[3.75rem]',
 };
 
 export default function HomeButton({ size = 'md', className, children }: BtnType) {
@@ -18,7 +18,7 @@ export default function HomeButton({ size = 'md', className, children }: BtnType
 
     return (
         <Link href="/about">
-            <button className={`${className} font-medium rounded-md bg-ourBlue text-white hover:bg-white hover:text-ourBlue ${shadowStyle} ${btnSize}`}>{children}</button>
+            <button className={`${className} font-medium rounded-md bg-ourBlue text-white hover:bg-white hover:text-ourBlue ${shadowStyle}`}>{children}</button>
         </Link>
     );
 }
