553801fac54a77029d8596d02162ef9c577e7e35
aiccuracy <soojin.emily@gmail.com>
2022-08-18 03:16:32 +0900

    refactor: remove unnecessary code

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -1,12 +1,8 @@
-import { Dispatch, SetStateAction } from 'react';
 import { IoIosCloseCircle } from 'react-icons/io';
-import { TechStack } from '../interfaces/techStack';
 
 interface TagParams {
     label: string;
     onRemove: (selectedLabel: string) => void;
-    // stacks: TechStack[] | undefined;
-    // setStacks: Dispatch<SetStateAction<TechStack[] | undefined>>;
 }
 
 function Tag({ label, onRemove }: TagParams) {
@@ -17,16 +13,7 @@ function Tag({ label, onRemove }: TagParams) {
                 onRemove(label);
             }}
         >
-            <IoIosCloseCircle
-                className="mr-1 fill-[#FFFFFF]"
-                // onClick={() => {
-                //     const index = labels.indexOf(x);
-                //     if (index > -1) {
-                //         labels.splice(index, 1);
-                //     }
-                //     setLabels(labels);
-                // }}
-            />
+            <IoIosCloseCircle className="mr-1 fill-[#FFFFFF]" />
             <p className="text-center text-[#FFFFFF] text-md">{label}</p>
         </div>
     );
