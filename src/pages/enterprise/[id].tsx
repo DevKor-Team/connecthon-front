adf8392ed8cb2a8be75f590d3ddae89ad5567ebc
Seungwoo Lee <timosean@naver.com>
2022-08-18 14:44:55 +0900

    feat: add initial Enterprise UI

--- src/pages/enterprise/[id].tsx
+++ src/pages/enterprise/[id].tsx
@@ -15,7 +15,24 @@ function Enterprise() {
                 console.log(e);
             }
         }
-    });
+
+        fetchCompany();
+    }, []);
+
+    return (
+        <main className="px-4 md:px-16 lg:px-20 xl:px-[13.375rem] bg-white md:bg-ourWhite mb-8 md:mb-0">
+            <div className="w-full pt-[6rem] md:py-[8rem] min-h-[100vh] h-auto">
+                <section className="flex flex-col justify-center items-start mb-3 md:mb-5">
+                    <h1 className="text-[1.35rem] leading-[1.55rem] md:leading-[2.813rem] md:text-[2.125rem] font-extrabold">참가자들에게 우리 기업을 홍보하고,</h1>
+                    <h1 className="text-[1.35rem] leading-[1.55rem] md:leading-[2.813rem] md:text-[2.125rem] font-extrabold">우수한 인재도 발굴해보세요!</h1>
+                </section>
+                <section className="flex flex-col justify-center items-start mb-12 md:mb-8">
+                    <p className="text-sm md:leading-[1.438rem] md:text-[0.938rem] text-ourGrey font-medium">입력하신 정보는 본 사이트 랜딩페이지에서 기업 로고 클릭 시</p>
+                    <p className="text-sm md:leading-[1.438rem] md:text-[0.938rem] text-ourGrey font-medium">해당 정보가 가공된 페이지로 이동되어 보여집니다. </p>
+                </section>
+            </div>
+        </main>
+    );
 }
 
 export default Enterprise;
