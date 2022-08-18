5367067065bf848b20c4289f5dff130a69f561e0
Seungwoo Lee <timosean@naver.com>
2022-08-18 13:42:53 +0900

    fix: router query

--- src/pages/enterprise/[id].tsx
+++ src/pages/enterprise/[id].tsx
@@ -2,7 +2,7 @@ import { useRouter } from 'next/router';
 
 function Enterprise() {
     const router = useRouter();
-    const { id } = router.query();
+    const { id } = router.query;
 }
 
 export default Enterprise;
