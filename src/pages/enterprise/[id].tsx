f51971223a65d77142f088f55b1298bf757746f2
Seungwoo Lee <timosean@naver.com>
2022-08-18 19:54:41 +0900

    fix: default height for introduction

--- src/pages/enterprise/[id].tsx
+++ src/pages/enterprise/[id].tsx
@@ -55,17 +55,17 @@ function Enterprise() {
                     <section className="w-full relative h-[33rem] pl-10 pr-10 py-10 md:pr-0 lg:px-10 xl:px-16 md:py-10 flex flex-col justify-center space-y-[2rem] bg-white rounded-2xl drop-shadow-[0px_0px_12px_rgba(32,135,255,0.1)] ">
                         <div className="flex flex-col">
                             <h4 className="font-bold text-xl">✏️한 줄 소개</h4>
-                            <span className="border-2 rounded-md md:w-[31.25rem] mt-2 p-1.5">{company?.profile?.introduction}</span>
+                            <span className="border-2 rounded-md md:w-[31.25rem] h-[2.5rem] mt-2 p-1.5">{company?.profile?.introduction || '-'}</span>
                         </div>
                         <div className="flex flex-col">
-                            <h4 className="font-bold text-xl break-words">💡저희는 아래와 같은 포지션을 찾고 있어요!</h4>
+                            <h4 className="font-bold text-xl break-words">💡저희는 다음과 같은 포지션을 모집하고 있어요!</h4>
                             <div className="flex flex-col space-y-3 md:flex-row md:space-x-1 md:items-center md:space-y-0 mt-2">
                                 <span className="border-2 rounded-md w-full md:w-[15rem] h-[2.5rem] p-1.5 mr-[1rem]">{company?.profile?.university || '-'}</span>
                                 <span className="border-2 rounded-md w-full md:w-[15rem] h-[2.5rem] p-1.5 mr-[1rem]">{company?.profile?.major || '-'}</span>
                             </div>
                         </div>
                         <div className="flex flex-col">
-                            <h4 className="font-bold text-xl mb-2 break-words">🎈저희는 이러한 사람들과 함께 하고 싶어요!</h4>
+                            <h4 className="font-bold text-xl mb-2 break-words">🎈저희는 이러한 분들과 함께 하고 싶어요!</h4>
                             <div className="flex flex-col space-y-3 items-start h-auto">
                                 {careers.map(crr => (
                                     <span className="border-2 rounded-md w-full md:w-[31.25rem] h-[2.5rem] p-1.5 mr-[1rem]">{crr}</span>
